9f258bc402ce4eebb68c51fbb919b439c2209569
matthisRipoche <matthisripoche@gmail.com>
2025-03-11 16:29:22 +0100

    test

--- routes/web.php
+++ routes/web.php
@@ -10,34 +10,34 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth')->group(function () {
 
-    Route::get('/', [DashboardController::class, 'index'])->name('dashboard.index');
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
 
     //route pour Restaurant
-    Route::get('/restaurants', [RestaurantController::class, 'index'])->name('restaurants.index');
-    Route::get('/restaurants/{id}/show', [RestaurantController::class, 'show'])->name('restaurants.show');
-    Route::get('/restaurants/create', [RestaurantController::class, 'create'])->name('restaurants.create');
-    Route::post('/restaurants', [RestaurantController::class, 'store'])->name('restaurants.store');
-    Route::get('/restaurants/{id}/edit', [RestaurantController::class, 'edit'])->name('restaurants.edit');
-    Route::put('/restaurants/{id}/update', [RestaurantController::class, 'update'])->name('restaurants.update');
-    Route::delete('/restaurants/{id}/destroy', [RestaurantController::class, 'destroy'])->name('restaurants.destroy');
+    Route::get('/dashboard/restaurants', [RestaurantController::class, 'index'])->name('restaurants.index');
+    Route::get('/dashboard/restaurants/{id}/show', [RestaurantController::class, 'show'])->name('restaurants.show');
+    Route::get('/dashboard/restaurants/create', [RestaurantController::class, 'create'])->name('restaurants.create');
+    Route::post('/dashboard/restaurants', [RestaurantController::class, 'store'])->name('restaurants.store');
+    Route::get('/dashboard/restaurants/{id}/edit', [RestaurantController::class, 'edit'])->name('restaurants.edit');
+    Route::put('/dashboard/restaurants/{id}/update', [RestaurantController::class, 'update'])->name('restaurants.update');
+    Route::delete('/dashboard/restaurants/{id}/destroy', [RestaurantController::class, 'destroy'])->name('restaurants.destroy');
 
     //route pour Category
-    Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
-    Route::get('/categories/{id}/show', [CategoryController::class, 'show'])->name('categories.show');
-    Route::get('/categories/create', [CategoryController::class, 'create'])->name('categories.create');
-    Route::post('/categories', [CategoryController::class, 'store'])->name('categories.store');
-    Route::get('/categories/{id}/edit', [CategoryController::class, 'edit'])->name('categories.edit');
-    Route::put('/categories/{id}/update', [CategoryController::class, 'update'])->name('categories.update');
-    Route::delete('/categories/{id}/destroy', [CategoryController::class, 'destroy'])->name('categories.destroy');
+    Route::get('/dashboard/categories', [CategoryController::class, 'index'])->name('categories.index');
+    Route::get('/dashboard/categories/{id}/show', [CategoryController::class, 'show'])->name('categories.show');
+    Route::get('/dashboard/categories/create', [CategoryController::class, 'create'])->name('categories.create');
+    Route::post('/dashboard/categories', [CategoryController::class, 'store'])->name('categories.store');
+    Route::get('/dashboard/categories/{id}/edit', [CategoryController::class, 'edit'])->name('categories.edit');
+    Route::put('/dashboard/categories/{id}/update', [CategoryController::class, 'update'])->name('categories.update');
+    Route::delete('/dashboard/categories/{id}/destroy', [CategoryController::class, 'destroy'])->name('categories.destroy');
 
     //route pour Item
-    Route::get('/items', [ItemController::class, 'index'])->name('items.index');
-    Route::get('/items/{id}/show', [ItemController::class, 'show'])->name('items.show');
-    Route::get('/items/create', [ItemController::class, 'create'])->name('items.create');
-    Route::post('/items', [ItemController::class, 'store'])->name('items.store');
-    Route::get('/items/{id}/edit', [ItemController::class, 'edit'])->name('items.edit');
-    Route::put('/items/{id}/update', [ItemController::class, 'update'])->name('items.update');
-    Route::delete('/items/{id}/destroy', [ItemController::class, 'destroy'])->name('items.destroy');
+    Route::get('/dashboard/items', [ItemController::class, 'index'])->name('items.index');
+    Route::get('/dashboard/items/{id}/show', [ItemController::class, 'show'])->name('items.show');
+    Route::get('/dashboard/items/create', [ItemController::class, 'create'])->name('items.create');
+    Route::post('/dashboard/items', [ItemController::class, 'store'])->name('items.store');
+    Route::get('/dashboard/items/{id}/edit', [ItemController::class, 'edit'])->name('items.edit');
+    Route::put('/dashboard/items/{id}/update', [ItemController::class, 'update'])->name('items.update');
+    Route::delete('/dashboard/items/{id}/destroy', [ItemController::class, 'destroy'])->name('items.destroy');
 });
 
 Route::middleware('auth')->group(function () {
